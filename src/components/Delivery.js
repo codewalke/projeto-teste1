534621f2651915865caf7d7387c46740ad3d0698
codewalke <116499647+codewalke@users.noreply.github.com>
2024-03-15 15:53:51 -0300

    primeiro commit

---
+++ src/components/Delivery.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Delevery = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick delivery App </h3>
+        <div className='w-[1240px] mx-auto grid md:grid-cols-2'>
+       <img className='w-[550px] mx-auto my-4 'src='Https://res.cloudinary.com/ehizeex-shop/image/upload/v1672676822/netflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp'/>
+        <div className='flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold'>Get The App</p>
+            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-demand</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan nisl a mauris gravida, ut imperdiet ipsum efficitur. 
+            Vestibulum sagittis, nulla eu condimentum aliquam, sapien justo convallis lectus, ut lacinia odio odio id lacus. 
+            Integer nec urna quis nunc bibendum tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+             Fusce dapibus mi sit amet metus consequat, sit amet dapibus nunc tincidunt.
+            In hac habitasse platea dictumst. Sed auctor, nunc vel feugiat tincidunt, erat augue consequat ex, vel bibendum felis neque eu elit.
+             Nullam nec vestibulum lectus. Vivamus vel tincidunt ex. 
+            Proin bibendum vehicula erat, eu fringilla odio malesuada vel. Aenean commodo velit vitae velit euismod, a aliquet elit faucibus
+            </p>
+            <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get started </button>
+
+        </div>
+        
+        </div>
+    </div>
+  )
+}
+
+
+export default Delevery
